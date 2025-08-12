5f0b23721db0b7dac81033dc2100eda345d5bb5e
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2025-08-12 09:36:12 +0530

    feat:Logging Aspect --> AOP

--- src/main/java/com/library/librarymanagement/aspect/LoggingAspect.java
+++ src/main/java/com/library/librarymanagement/aspect/LoggingAspect.java
@@ -1,21 +1,34 @@
 package com.library.librarymanagement.aspect;
 
-import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 @Component
 public class LoggingAspect {
+    @Before("execution(* com.library.librarymanagement.service.*.*(..))")
+    public void BeforeService()
+    {
+        System.out.println("Before service");
+    }
 
-    @Around("execution(* com.library.service.*.*(..))")
+    @Around("execution(* com.library.librarymanagement.service.*.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+
+
         long start = System.currentTimeMillis();
         Object proceed = joinPoint.proceed(); // execute the method
         long timeTaken = System.currentTimeMillis() - start;
         System.out.println("Execution of " + joinPoint.getSignature() + " took " + timeTaken + " ms");
         return proceed;
+    }
+
+    @After("execution(* com.library.librarymanagement.service.*.*(..))")
+    public void AfterService()
+    {
+        System.out.println("After Service");
     }
 }
